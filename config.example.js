42ecd9c22c0ed720fb85be50d4b2c6823052f1a8
Hayden Brown <nedyah.brown@gmail.com>
2021-02-14 15:32:58 +0000

    config: Fixed sample config file

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 64, // recommended: leave this at it's current value
-    clientIds: [""], // client id's go here. if you don't know them, play a song on your client and it will be logged
+    clientIds: [], // client id's go here. if you don't know them, play a song on your client and it will be logged
     user: "username", // username of the user you want to track 
     discord: {
         clientId: "810213130453778473", // leave this default if you want a plex status
